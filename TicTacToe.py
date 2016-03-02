0f665adfe0393758905eb8847fb11c51cdaf0597
AuraStormLucario <yolo2infinity@icloud.com>
2016-03-02 08:59:11 -0500

    Fix cpu_move_turn_three()

--- TicTacToe.py
+++ TicTacToe.py
@@ -166,7 +166,7 @@ def cpu_move_turn_three(turn): # cpu move for turn 2
 		S[moveC * 2] = order[turn]
 		print "The Computer will go on space",(moveC * 2) + 1,"index",moveC * 2
 	if S[4] == player: # if player is center, cpu moves in opposite corner from turn 0
-		for moveC in range(0, 5):
+		for moveC in range(0, 4):
 			if S[moveC * 2] == cpu and S[8 - (moveC * 2)] == empty: # code block can only run once
 				S[8 - (moveC * 2)] = order[turn]
 				print "The Computer will go on space",9 - (moveC * 2),"index",8 - (moveC * 2)
